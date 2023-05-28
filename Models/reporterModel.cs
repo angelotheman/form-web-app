da95665360e055fa0b7a1b1442684652183cc403
angelotheman <ktwum721@gmail.com>
2023-05-28 19:50:18 +0000

    Test Files Added to the Site

---
+++ Models/reporterModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace formApp.Models;
+public class Address
+{
+    [Key]
+    public int Id { set; get; }
+    public string? First_name { set; get; }
+    public string? Last_name { set; get; }
+    public string? Email { set; get; }
+    public int Contact { set ; get; }
+    /*[DataType(DataType.Upload)]
+    public HttpPostedFileBase ImageFile { set; get; }
+    */
+}

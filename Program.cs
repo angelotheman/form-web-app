5fe57661b3fa5292811f074ca3251b9938527010
angelotheman <ktwum721@gmail.com>
2023-05-30 12:56:45 +0000

    Files added for configuration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 

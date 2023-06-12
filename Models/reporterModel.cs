7cd5389a21f838c473e997421aef37a709f4fb88
Kwabena Twumasi <61213902+angelotheman@users.noreply.github.com>
2023-06-12 07:56:20 +0000

    Set up models updates

--- Models/reporterModel.cs
+++ Models/reporterModel.cs
@@ -1,16 +1,26 @@
 using System.ComponentModel.DataAnnotations;
 using System.Web;
 
-namespace formApp.Models;
-public class Address
+namespace formApp.Models
 {
-    [Key]
-    public int Id { set; get; }
-    public string? First_name { set; get; }
-    public string? Last_name { set; get; }
-    public string? Email { set; get; }
-    public int Contact { set ; get; }
-    /*[DataType(DataType.Upload)]
-    public HttpPostedFileBase ImageFile { set; get; }
-    */
+    public class Address
+    {
+        [Key]
+        public int Id { set; get; }
+        public string? First_name { set; get; }
+        public string? Last_name { set; get; }
+        public string? Email { set; get; }
+        public int Contact { set ; get; }
+        /*[DataType(DataType.Upload)]
+        public HttpPostedFileBase ImageFile { set; get; }
+        */
+        public Categories FaultTypes { set; get; }
+    }
+
+    public enum Categories {
+        AirCondition,
+        Painting,
+        Materials,
+        Fittings
+    }
 }
